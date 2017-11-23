f0d4751072fa497a0b487c6db1f301a3cdd8e9e9
GiggleLiu <cacate0129@gmail.com>
2017-11-24 01:43:07 +0800

    update program.

--- utils.py
+++ utils.py
@@ -1,52 +1,80 @@
 import numpy as np
+import pdb
 import tensorflow as tf
 
-__all__ = ['sample_prob', 'typed_normal', 'save_img44']
+__all__ = ['sample_prob', 'typed_normal', 'save_img44', 'convolution_pbc']
+
 
 def sample_prob(probs, name):
     '''give 1 with specific probability, 0 otherwise.'''
     with tf.variable_scope(name):
-        return tf.nn.relu(tf.sign(probs - tf.cast(tf.random_uniform(probs.shape, 0,1), probs.dtype)))
+        return tf.nn.relu(tf.sign(probs - tf.cast(tf.random_uniform(tf.shape(probs), 0, 1), probs.dtype)))
+
 
 def typed_normal(dtype, shape, scale):
     '''normal distribution with specific data type.'''
     dtype = dtype.name
     if dtype in ['complex64', 'complex128']:
-        arr = np.random.normal(0,scale,shape) + 1j*np.random.normal(0,scale,shape)
+        arr = np.random.normal(0, scale, shape) + 1j * \
+            np.random.normal(0, scale, shape)
     else:
         arr = np.random.normal(0, scale, shape)
     return arr.astype(dtype)
 
+
 def save_img44(filename, data):
     import matplotlib.pyplot as plt
     nrow, ncol = 4, 4
-    gs = plt.GridSpec(nrow,ncol)
-    fig = plt.figure(figsize = (6,5))
+    gs = plt.GridSpec(nrow, ncol)
+    fig = plt.figure(figsize=(6, 5))
     for i in range(nrow):
         for j in range(ncol):
-            ax = plt.subplot(gs[i,j])
-            ax.imshow(data[i*ncol+j], interpolation='none', cmap='Greys')
+            ax = plt.subplot(gs[i, j])
+            ax.imshow(data[i * ncol + j], interpolation='none', cmap='Greys')
             ax.axis('equal')
             ax.axis('off')
     gs.tight_layout(fig, pad=0)
     plt.show()
+
 
 def binning_statistics(var_list, num_bin):
     '''
     binning statistics for variable list.
     '''
     num_sample = len(var_list)
-    if num_sample%num_bin!=0:
+    var_list = var_list.real
+    if num_sample % num_bin != 0:
         raise
-    size_bin = num_sample//num_bin
+    size_bin = num_sample // num_bin
 
     # mean, variance
     mean = np.mean(var_list, axis=0)
-    variance= np.var(var_list, axis=0)
+    variance = np.var(var_list, axis=0)
 
     # binned variance and autocorrelation time.
-    variance_binned = np.var([np.mean(var_list[size_bin*i:size_bin*(i+1)]) for i in range(num_bin)])
-    t_auto = 0.5 * size_bin * np.mean(variance_binned) / np.mean(variance)
-    stderr = abs(np.sqrt(variance_binned / num_bin))
-    print('Energy = %.4f +- %.4f, Auto correlation Time = %.4f'%(mean, stderr, t_auto))
+    variance_binned = np.var(
+        [np.mean(var_list[size_bin * i:size_bin * (i + 1)]) for i in range(num_bin)])
+    t_auto = 0.5 * size_bin * \
+        np.abs(np.mean(variance_binned) / np.mean(variance))
+    stderr = np.sqrt(variance_binned / num_bin)
+    print('Binning Statistics: Energy = %.4f +- %.4f, Auto correlation Time = %.4f' %
+          (mean, stderr, t_auto))
     return mean, stderr
+
+
+def get_variable_by_name(name):
+    weight_var = tf.get_collection(tf.GraphKeys.VARIABLES, name)[0]
+
+
+def convolution_pbc(input, filter, *args, data_format=None, **kwargs):
+    '''convolution with periodic padding.'''
+    irank = len(input.shape) - 2
+    if data_format is None or data_format[:2] != 'NC':
+        expanded_input = tf.tile(
+            input, [1] + [2] * irank + [1])[(slice(None),) + (slice(None, -1),) * irank]
+    else:
+        expanded_input = tf.tile(
+            input, [1, 1] + [2] * irank)[(slice(None),) * 2 + (slice(None, -1),) * irank]
+    res = tf.nn.convolution(expanded_input, filter, padding='VALID',
+                            data_format=data_format, *args, **kwargs)
+    return res
--- vmc.py
+++ vmc.py
@@ -4,12 +4,29 @@ Variational Monte Carlo Kernel.
 from __future__ import division
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import pdb
 
 from utils import binning_statistics
 from rbm_tf_cd import load_demo_rbm
 
-def vmc_measure(model, initial_config, num_bath=200, num_sample=1000, num_bin=50, measure_step=5):
+
+def train(model, max_iter=2000):
+    '''train a model.'''
+    E_list, E_err_list = [], []
+    for i in range(max_iter):
+        E_mean, grad_mean, Egrad_mean, E_err = vmc_measure(
+            model, initial_config=np.array([0, 1] * (model.nsite // 2)), num_sample=500)
+        print('Step %d, dE = %.4f' % (i, E_mean - model.E_exact))
+        model.update_vars(E_mean, grad_mean, Egrad_mean)
+        print(model.get_wf(np.array([1, 0] * (model.nsite // 2))),
+              model.get_wf(np.array([0, 1] * (model.nsite // 2))))
+        E_list.append(E_mean)
+        E_err_list.append(E_err)
+    return E_list, E_err_list
+
+
+def vmc_measure(model, initial_config, num_bath=200, num_sample=1000, num_bin=50, measure_step=None):
     '''
     Measure an operator.
 
@@ -20,18 +37,20 @@ def vmc_measure(model, initial_config, num_bath=200, num_sample=1000, num_bin=50
     Return:
         number,
     '''
-    n_accepted = 0
-    print_step = num_sample*measure_step/5
+    if measure_step is None:
+        measure_step = len(initial_config)
+    print_step = num_sample * measure_step / 5
 
     E_locs, grad_locs = [], []
     config = initial_config
     wf = model.get_wf(config)
 
-    for i in range(num_bath + num_sample*measure_step):
+    n_accepted = 0
+    for i in range(num_bath + num_sample * measure_step):
         # generate new config and calculate probability ratio
-        config_proposed = model.propose_config(config)
+        config_proposed = propose_config(config)
         wf_proposed = model.get_wf(config_proposed)
-        prob_ratio = np.abs(wf_proposed/wf).item()**2
+        prob_ratio = np.abs(wf_proposed / wf).item()**2
 
         # accept/reject move by one-line metropolis algorithm
         if np.random.random() < prob_ratio:
@@ -40,15 +59,16 @@ def vmc_measure(model, initial_config, num_bath=200, num_sample=1000, num_bin=50
             n_accepted += 1
 
         # measurements
-        if i >= num_bath and i%measure_step==0:
+        if i >= num_bath and i % measure_step == 0:
             # here, I choose a lazy way that re-compute on this config, in order to get its gradients easily.
             E_loc, grad_loc = model.local_measure(config)
             E_locs.append(E_loc)
             grad_locs.append(grad_loc)
 
         # print status
-        if i%print_step == print_step-1:
-            print('%-10s Accept rate: %.3f' % (i + 1, n_accepted*1. / print_step))
+        if i % print_step == print_step - 1:
+            print('%-10s Accept rate: %.3f' %
+                  (i + 1, n_accepted * 1. / print_step))
             n_accepted = 0
 
     # process samples
@@ -57,63 +77,82 @@ def vmc_measure(model, initial_config, num_bath=200, num_sample=1000, num_bin=50
 
     grad_mean = []
     Egrad_mean = []
+    gradgrad_mean = []
     for grad_locs_i in zip(*grad_locs):
-        grad_mean.append(np.mean(grad_locs_i, axis=0))
-        Egrad_mean.append(np.mean(E_locs*grad_locs_i))
-    return E_mean.item(), grad_mean, Egrad_mean, E_err.item()
+        grad_locs_i = np.array(grad_locs_i)
+        grad_mean.append(grad_locs_i.mean(axis=0))
+        Egrad_mean.append(
+            (E_locs[(slice(None),) + (None,) * (grad_locs_i.ndim - 1)] * grad_locs_i).mean(axis=0))
+    return E_mean, grad_mean, Egrad_mean, E_err
+
 
 class Model(object):
-    def __init__(self, J, rbm, sess, learning_rate=0.01):
+    def __init__(self, J, rbm, sess, E_exact, learning_rate=0.01, use_cnn=True):
         self.J = J
         self.sess = sess
         self.nsite = rbm.visible_input.get_shape()[1].value
         self.rbm = rbm
         self.learning_rate = learning_rate
-        self.optimizer = tf.train.AdamOptimizer(learning_rate)
+        # self.optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        self.E_exact = E_exact
 
         # build graphs
-        self.wf_graph = rbm.build_graph('v-prob')
-        self.var_list=[rbm.weights,rbm.hidden_bias,rbm.visible_bias]
-        self.gradients = tf.gradients(xs=self.var_list, ys=tf.log(self.wf_graph))
-        sess.run(tf.global_variables_initializer())
+        if use_cnn:
+            self.wf_graph = rbm.build_graph('psi-cnn')
+            self.var_list = [rbm.weights, rbm.hidden_bias]
+        else:
+            self.wf_graph = rbm.build_graph('psi')
+            self.var_list = [rbm.weights, rbm.hidden_bias, rbm.visible_bias]
+        self.gradients = tf.gradients(
+            xs=self.var_list, ys=tf.log(self.wf_graph))
 
     def get_wf(self, config):
-        wf = self.sess.run(self.wf_graph, feed_dict={self.rbm.visible_input:config[None,:]})
+        wf = self.sess.run(self.wf_graph, feed_dict={
+                           self.rbm.visible_input: config[None, :]})
         return wf
 
     def local_measure(self, config):
         '''get E_loc, grad_loc.'''
         # {d/dW}_{loc}
-        res = self.sess.run([self.wf_graph]+self.gradients, feed_dict={self.rbm.visible_input:config[None,:]})
+        res = self.sess.run([self.wf_graph] + self.gradients,
+                            feed_dict={self.rbm.visible_input: config[None, :]})
         wf = res[0]
         grad_locs = res[1:]
 
         # E_{loc}
-        E_loc = heisenberg_loc(self.J, config, self.get_wf, wf)
+        E_loc = heisenberg_loc(self.J, config, self.get_wf, wf).item()
 
         return E_loc, grad_locs
 
     def update_vars(self, E_mean, grad_mean, Egrad_mean):
-        g_list = [eg - E_mean*g for eg,g in zip(Egrad_mean, grad_mean)]  # no conjugate here
+        g_list = [eg - E_mean * g for eg,
+                  g in zip(Egrad_mean, grad_mean)]  # no conjugate here
         # assign new values
-        assign_ops = [var.assign(var-self.learning_rate*g) for var,g in zip(self.var_list, g_list)]
+        assign_ops = [var.assign(var - self.learning_rate * g)
+                      for var, g in zip(self.var_list, g_list)]
         #assign_ops = self.optimizer.apply_gradients(zip(g_list, self.var_list))
         self.sess.run(assign_ops)
 
-    def propose_config(self, old_config):
-        '''
-        flip two positions as suggested spin flips.
-        '''
-        nsite = len(old_config)
-        upmask=old_config==1
-        flips=np.random.randint(0,nsite//2,2)
-        iflip0=np.where(upmask)[0][flips[0]]
-        iflip1=np.where(~upmask)[0][flips[1]]
 
-        config = old_config.copy()
-        config[iflip0] = -1
-        config[iflip1] = 1
-        return config
+def propose_config(old_config, prob_flip=0.05):
+    '''
+    flip two positions as suggested spin flips.
+    '''
+    # take ~ 5% probability to flip all spin, can making VMC sample better in Heisenberg model
+    if np.random.random() < prob_flip:
+        return 1-old_config
+
+    nsite = len(old_config)
+    upmask = old_config == 0
+    flips = np.random.randint(0, nsite // 2, 2)
+    iflip0 = np.where(upmask)[0][flips[0]]
+    iflip1 = np.where(~upmask)[0][flips[1]]
+
+    config = old_config.copy()
+    config[iflip0] = 1
+    config[iflip1] = 0
+    return config
+
 
 def heisenberg_loc(J, config, wave_func, wf0):
     '''
@@ -123,7 +162,7 @@ def heisenberg_loc(J, config, wave_func, wf0):
     nsite = len(config)
     wl, flips = [], []
     # J*SzSz terms.
-    nn_par = np.roll(config, -1) * config
+    nn_par = 1 - 2. * (np.roll(config, -1) ^ config)
     wl.append(J / 4. * (nn_par).sum(axis=-1))
     flips.append(np.array([], dtype='int64'))
 
@@ -138,30 +177,24 @@ def heisenberg_loc(J, config, wave_func, wf0):
     acc = 0
     for wi, flip in zip(wl, flips):
         config_i = config.copy()
-        config_i[list(flip)] *= -1
-        eng_i=wi * wave_func(config_i) / wf0
+        config_i[list(flip)] = 1 - config_i[list(flip)]
+        eng_i = wi * wave_func(config_i) / wf0
         acc += eng_i
     return acc
 
-def train(model, max_iter=2000):
-    '''train a model.'''
-    E_list, E_err_list = [], []
-    for i in range(max_iter):
-        E_mean, grad_mean, Egrad_mean, E_err = vmc_measure(model, initial_config=np.array([-1,1]*(model.nsite//2)), num_sample=1000)
-        print('Step %d, energy = %s'%(i, E_mean))
-        model.update_vars(E_mean, grad_mean, Egrad_mean)
-        E_list.append(E_mean)
-        E_err_list.append(E_err)
-    return E_list, E_err_list
 
 def run_demo():
+    tf.set_random_seed(10087)
+    np.random.seed(10086)
     rbm = load_demo_rbm('spin-chain')
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
-        wf_graph = rbm
-        model = Model(J=1.0, rbm=rbm, sess=sess, learning_rate=0.05)
-        E_list, E_err_list = train(model, 200)
-    plt.plot(E_list)
+        sess.run(tf.global_variables_initializer())
+        model = Model(J=1.0, rbm=rbm, sess=sess, E_exact=-
+                      3.65109341, learning_rate=0.1)
+        E_list, E_err_list = train(model, max_iter=200)
+    plt.errorbar(arange(1, len(E_list) + 1), E_list, yerr=E_err_list)
+
 
 if __name__ == '__main__':
     run_demo()
