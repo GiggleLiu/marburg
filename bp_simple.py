426e3d389c548cb01f5cebff5075cdb9179b67de
Li SH <li012589li@outlook.com>
2017-12-01 00:27:13 +0800

    it seems works

--- bp_simple.py
+++ bp_simple.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import struct
+
+np.random.seed(42)
 
 def unpack(filename):
     with open(filename,'rb') as f:
@@ -35,39 +38,49 @@ def MSE_backward(delta,x):
     return delta*(x)
 
 def main():
-    Batch_Size = 16
+    Batch_Size = 5
     Steps = 100
     IMG_Size = 28*28
-    Hidden_Size_1 = 42
+    Hidden_Size_1 = 110
     Hidden_Size_2 = 10
     Learning_Rate = 0.001
     Maximum = 10000
-    Nepochs = 10
+    Nepochs = 5
 
     data = load_MNIST()
 
-    for i in range(Nepochs):
 
-        for i in range(Maximum//Batch_Size):
-            train = data[0].reshape([-1,IMG_Size])[:Batch_Size,:]
-            labels = data[1][:Batch_Size,:]
-
-            weight_1 = np.random.randn(IMG_Size,Hidden_Size_1)
-            bias_1 = np.random.randn(Hidden_Size_1)
-
-            weight_2 = np.random.randn(Hidden_Size_1,Hidden_Size_2)
-            bias_2 = np.random.randn(Hidden_Size_2)
-
+    weight_1 = np.random.randn(IMG_Size,Hidden_Size_1)
+    bias_1 = np.random.randn(Hidden_Size_1)
+    
+    weight_2 = np.random.randn(Hidden_Size_1,Hidden_Size_2)
+    bias_2 = np.random.randn(Hidden_Size_2)
+    for _ in range(Nepochs):
+        for i in range(100):
+            #import pdb
+            #pdb.set_trace()
+            train = data[0].reshape([-1,IMG_Size])[Batch_Size*(i):Batch_Size*(i+1),:]
+            labels = data[1][Batch_Size*(i):Batch_Size*(i+1),:]
+            '''
+            if i%20 == 0:
+                print(labels[0,:])
+                print(train[0].reshape([28,28]).shape)
+                plt.imshow(train[0].reshape([28,28]))
+                plt.show()
+            '''
             layer_1_ = train.dot(weight_1)+bias_1
-            layer_1 = sigmoid(layer_1)
+            layer_1 = sigmoid(layer_1_)
             layer_2_ = layer_1.dot(weight_2)+bias_2
-            layer_2 = sigmoid(layer_2)
+            layer_2 = sigmoid(layer_2_)
 
             loss = MSE(layer_2,labels)
+            print(layer_2)
+            print(labels)
+            #print((layer_2-labels)**2)
             print(loss)
 
             delta = Learning_Rate
-            delta = MSE_backward(delta,loss)
+            delta = MSE_backward(delta,layer_2-labels)
             delta = sigmoid_backward(delta,layer_2)
             delta_weight_2 = layer_1.T.dot(delta)
             delta_bias_2 = np.sum(delta,0)
@@ -77,7 +90,7 @@ def main():
             delta_weight_1 = train.T.dot(delta)
             delta_bias_1 = np.sum(delta,0)
 
-            weight_1 += delta_weight_1
-            weight_2 += delta_weight_2
+            weight_1 -= delta_weight_1
+            weight_2 -= delta_weight_2
 if __name__ == "__main__":
     main()
