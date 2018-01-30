78deb9347563fd8ba7d8ab0e895d36f568ae1949
Li SH <li012589li@outlook.com>
2018-01-30 22:43:22 +0800

    change buff

--- bp/utils.py
+++ bp/utils.py
@@ -25,48 +25,14 @@ def load_MNIST():
     data[3] = labels
     return data
 
-def normalization(data):
-    return data+np.random.uniform(size=data.shape)/256.0
-
 class Buffer(object):
-    def __init__(self,maximum,data=None,testRatio=0.0):
-        testSize = int(testRatio*maximum)
-        self.testRatio = testRatio
-        self.capacity = int(maximum*(1+self.testRatio))
+    def __init__(self,data,label):
         self.data=data
-        if data is None:
-            self.maximum = 0
-        else:
-            self.maximum = len(data)
+        self.label = label
+        assert data.shape[0] == label.shape[0]
 
-    def draw(self,batchSize,testSize=None):
-        maximum = int(self.maximum*(1.0-self.testRatio))
-        if batchSize > maximum:
-            batchSize = maximum
-        perm = np.random.permutation(maximum)
-        if testSize is None:
-            return self.data[perm[:batchSize]]
-        else:
-            if testSize > self.maximum-maximum:
-                testSize = self.maximum-maximum
-            permp = np.random.permutation(self.maximum-maximum)+maximum
-            train = self.data[perm[:batchSize]]
-            test = self.data[permp[-testBatchSize:-1]]
-            return train, test
-    def drawtest(self,testSize):
-        if testSize > int(self.maximum*(self.testRatio)):
-            testSize = int(self.maximum*(self.testRatio))
-        perm = np.random.permutation(self.maximum-maximum)+maximum
-        test = self.data[perm]
-        return test
-    def push(self,data):
-        if self.data is None:
-            self.data = data
-        else:
-            self.data = torch.cat((self.data,data),0)
-        if self.data.shape[0] > self.capacity:
-            self._maintain()
-        self.maximum = len(self.data)
-    def _maintain(self):
+    def draw(self,batchSize):
         perm = np.random.permutation(self.data.shape[0])
-        self.data = self.data[perm[:self.capacity]]
+        data = self.data[perm[:batchSize]]
+        label = self.label[perm[:batchSize]]
+        return data, label
