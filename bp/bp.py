ccaea2186cc2dbb9ab6b4b108f9bef9119c983ca
Li SH <li012589li@outlook.com>
2018-01-30 22:41:44 +0800

    add network class

--- bp/bp.py
+++ bp/bp.py
@@ -1,87 +1,158 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import struct
+from utils import load_MNIST
+from utils import Buffer
 
-def unpack(filename):
-    with open(filename,'rb') as f:
-        _,_, dims = struct.unpack('>HBB',f.read(4))
-        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
-        data = np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
-        return data
-
-def load_MNIST():
-    files = ['MNIST/t10k-images-idx3-ubyte','MNIST/t10k-labels-idx1-ubyte','MNIST/train-images-idx3-ubyte','MNIST/train-labels-idx1-ubyte']
-    data = []
-    for name in files:
-        data.append(unpack(name))
-    labels = np.zeros([data[1].shape[0],10])
-    for i,iterm in enumerate(data[1]):
-        labels[i][iterm] = 1
-    data[1] = labels
-    labels = np.zeros([data[3].shape[0],10])
-    for i,iterm in enumerate(data[3]):
-        labels[i][iterm] = 1
-    data[3] = labels
-    return data
 
 class Sigmoid(object):
-    def __init__(self):
-        pass
     def forward(self,x):
         self.x = x
-        return 1.0/(1.0+np.exp(-x))
+        self.r =  1.0/(1.0+np.exp(-x))
+        return self.r
     def backward(self,delta):
-        return delta.dot((1-self.forward(self.x))*self.forward(self.x))
-    def update(self):
-        pass
+        return delta*((1-self.r)*self.r)
 
 class MSE(object):
-    def __init__(self):
-        pass
     def forward(self,x,l):
         self.x = x
         self.l = l
         return 0.5*((x-l)**2).sum()
     def backward(self,delta):
         return delta*(self.x-self.l)
-    def update(self):
-        pass
 
-class MLP_layer(object):
-    def __init__(self,input_shape,output_shape,activation):
+class Linear(object):
+    def __init__(self,input_shape,output_shape):
         self.weight = np.random.randn(input_shape,output_shape)
         self.bias = np.random.randn(output_shape)
-        self.activation = activation
     def forward(self,x):
         self.x = x
-        return self.activation(np.matmul(x,self.weight)+self.bias)
+        return np.matmul(x,self.weight)+self.bias
     def backward(self,delta):
-        delta = self.activation.backward(delta)
         self.weight_delta = self.x.T.dot(delta)
         self.bias_delta = np.sum(delta,0)
-        return self.delta  # incorrect?
-    def update(self):
-        self.weight -= self.weight_delta
-        self.bias -= self.bias_delta
+        return delta.dot(self.weight.T)
 
 class Network(object):
-    def __init__(self,layers_list):
-        self.layers_list = self.layers_list
+    def __init__(self,layers_list,loss):
+        self.layers_list = layers_list
+        self.lossfn = loss
     def forward(self,x):
         for layer in self.layers_list:
             x = layer.forward(x)
         return x
-    def backward(self,x,learning_rate):
-        delta = learning_rate
+    def loss(self,x,label):
+        x = self.forward(x)
+        l = self.lossfn.forward(x,label)
+        return l
+    def backward(self,learning_rate):
+        delta = self.lossfn.backward(learning_rate)
         for layer in reversed(self.layers_list):
             delta = layer.backward(delta)
     def update(self):
         for layer in self.layers_list:
-            layer.update()
+            if type(layer) == Linear:
+                layer.weight -= layer.weight_delta
+                layer.bias -= layer.bias_delta
+
+def normalization(data):
+    return (data+np.random.uniform(size=data.shape))/256.0
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    group = parser.add_argument_group('learning  parameters')
+    group.add_argument("-epochs",type=int,default=10,help="epochs to run")
+    group.add_argument("-batchsize",type=int,default=300,help="size of batch to train")
+    group.add_argument("-lr", type=float, default=0.001, help="learning rate")
+    group.add_argument("-iterations", type = int,default=-1,help="num of itrations in each epoch")
+
+    group = parser.add_argument_group('network structure')
+    group.add_argument("-layers",type=int,default=5,help="layers in network")
+    group.add_argument("-hidden",type=int,default=1000,help="hidden features in each layer")
+
+    group = parser.add_argument_group('test parameters')
+    group.add_argument("-testbatch",type=int,default=100,help="num of test samples")
+    args = parser.parse_args()
+
     data = load_MNIST()
+    buff = Buffer(data[2],data[3])
+    testbuff = Buffer(data[0],data[1])
+
+    l1 = Linear(784,500)
+    l2 = Linear(500,100)
+    l3 = Linear(100,10)
+
+    activation1 = Sigmoid()
+    activation2 = Sigmoid()
+    activation3 = Sigmoid()
+
+    mse = MSE()
+
+    network = Network([l1,activation1,l2,activation2,l3,activation3],mse)
+
+    if args.iterations == -1:
+        iterations = buff.data.shape[0]//args.batchsize
+    else:
+        iterations = args.iterations
+
+    for i in range(args.epochs):
+        for j in range(iterations):
+            train,label = buff.draw(args.batchsize)
+
+            train = normalization(train)
+
+            l = network.loss(train.reshape(train.shape[0],-1),label)
+            '''
+            tmp1 = l1.forward(train.reshape(train.shape[0],-1))
+            act1 = activation1.forward(tmp1)
+            tmp2 = l2.forward(act1)
+            act2 = activation2.forward(tmp2)
+            tmp3 = l3.forward(act2)
+            act3 = activation3.forward(tmp3)
+
+            l = mse.forward(act3,label)
+            '''
+
+            print("epoch:",i,"iteration:",j,"/",iterations,"loss:",l)
+
+            network.backward(args.lr)
+            network.update()
+            '''
+            mseb = mse.backward(args.lr)
+
+            act3b = activation3.backward(mseb)
+            l3b = l3.backward(act3b)
+            act2b = activation2.backward(l3b)
+            l2b = l2.backward(act2b)
+            act1b = activation1.backward(l2b)
+            l1b = l1.backward(act1b)
+
+            l3.weight -= l3.weight_delta
+            l3.bias -= l3.bias_delta
+            l2.weight -= l2.weight_delta
+            l2.bias -= l2.bias_delta
+            l1.weight -= l1.weight_delta
+            l1.bias -= l1.bias_delta
+            '''
+
+    import pdb
+    pdb.set_trace()
+
+    test,label = testbuff.draw(args.testbatch)
+
+    test = normalization(test)
+
+    tmp1 = l1.forward(test.reshape(test.shape[0],-1))
+    act1 = activation1.forward(tmp1)
+    tmp2 = l2.forward(act1)
+    act2 = activation2.forward(tmp2)
+    tmp3 = l3.forward(act2)
+    act3 = activation3.forward(tmp3)
+
+    l = mse.forward(act3,label)
+
+    print(l)
 
 if __name__ == "__main__":
     main()
