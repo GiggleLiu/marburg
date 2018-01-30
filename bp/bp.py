6dfd7bebcdba43cdab5b05b5c46f2887ad0fc48f
Li SH <li012589li@outlook.com>
2018-01-30 15:31:32 +0800

    mv bp related to floder bp

---
+++ bp/bp.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import struct
+
+def unpack(filename):
+    with open(filename,'rb') as f:
+        _,_, dims = struct.unpack('>HBB',f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        data = np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
+        return data
+
+def load_MNIST():
+    files = ['MNIST/t10k-images-idx3-ubyte','MNIST/t10k-labels-idx1-ubyte','MNIST/train-images-idx3-ubyte','MNIST/train-labels-idx1-ubyte']
+    data = []
+    for name in files:
+        data.append(unpack(name))
+    labels = np.zeros([data[1].shape[0],10])
+    for i,iterm in enumerate(data[1]):
+        labels[i][iterm] = 1
+    data[1] = labels
+    labels = np.zeros([data[3].shape[0],10])
+    for i,iterm in enumerate(data[3]):
+        labels[i][iterm] = 1
+    data[3] = labels
+    return data
+
+class Sigmoid(object):
+    def __init__(self):
+        pass
+    def forward(self,x):
+        self.x = x
+        return 1.0/(1.0+np.exp(-x))
+    def backward(self,delta):
+        return delta.dot((1-self.forward(self.x))*self.forward(self.x))
+    def update(self):
+        pass
+
+class MSE(object):
+    def __init__(self):
+        pass
+    def forward(self,x,l):
+        self.x = x
+        self.l = l
+        return 0.5*((x-l)**2).sum()
+    def backward(self,delta):
+        return delta*(self.x-self.l)
+    def update(self):
+        pass
+
+class MLP_layer(object):
+    def __init__(self,input_shape,output_shape,activation):
+        self.weight = np.random.randn(input_shape,output_shape)
+        self.bias = np.random.randn(output_shape)
+        self.activation = activation
+    def forward(self,x):
+        self.x = x
+        return self.activation(np.matmul(x,self.weight)+self.bias)
+    def backward(self,delta):
+        delta = self.activation.backward(delta)
+        self.weight_delta = self.x.T.dot(delta)
+        self.bias_delta = np.sum(delta,0)
+        return self.delta  # incorrect?
+    def update(self):
+        self.weight -= self.weight_delta
+        self.bias -= self.bias_delta
+
+class Network(object):
+    def __init__(self,layers_list):
+        self.layers_list = self.layers_list
+    def forward(self,x):
+        for layer in self.layers_list:
+            x = layer.forward(x)
+        return x
+    def backward(self,x,learning_rate):
+        delta = learning_rate
+        for layer in reversed(self.layers_list):
+            delta = layer.backward(delta)
+    def update(self):
+        for layer in self.layers_list:
+            layer.update()
+
+def main():
+    data = load_MNIST()
+
+if __name__ == "__main__":
+    main()
---
+++ bp/bp_simple.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import struct
+
+#np.random.seed(42)
+
+def unpack(filename):
+    with open(filename,'rb') as f:
+        _,_, dims = struct.unpack('>HBB',f.read(4))
+        shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        data = np.fromstring(f.read(), dtype=np.uint8).reshape(shape)
+        return data
+
+def load_MNIST():
+    files = ['MNIST/t10k-images-idx3-ubyte','MNIST/t10k-labels-idx1-ubyte','MNIST/train-images-idx3-ubyte','MNIST/train-labels-idx1-ubyte']
+    data = []
+    for name in files:
+        data.append(unpack(name))
+    labels = np.zeros([data[1].shape[0],10])
+    for i,iterm in enumerate(data[1]):
+        labels[i][iterm] = 1
+    data[1] = labels
+    labels = np.zeros([data[3].shape[0],10])
+    for i,iterm in enumerate(data[3]):
+        labels[i][iterm] = 1
+    data[3] = labels
+    return data
+
+def sigmoid(x):
+    return 1.0/(1.0+np.exp(-x))
+
+def sigmoid_backward(delta,x):
+    return delta*((1-sigmoid(x))*sigmoid(x))
+
+def MSE(x,l):
+    return 0.5*((x-l)**2).mean()
+def MSE_backward(delta,x):
+    return delta*(x)
+
+def main():
+    Batch_Size = 10
+    Steps = 100
+    IMG_Size = 28*28
+    Hidden_Size_1 = 110
+    Hidden_Size_2 = 10
+    Learning_Rate = 0.001
+    Maximum = 10000
+    Nepochs = 5
+
+    data = load_MNIST()
+
+
+    weight_1 = np.random.randn(IMG_Size,Hidden_Size_1)
+    bias_1 = np.random.randn(Hidden_Size_1)
+
+    weight_2 = np.random.randn(Hidden_Size_1,Hidden_Size_2)
+    bias_2 = np.random.randn(Hidden_Size_2)
+
+    loss_pack = []
+    ratio_pack = []
+    for _ in range(Nepochs):
+        for i in range(Steps):
+            #import pdb
+            #pdb.set_trace()
+            train = data[0].reshape([-1,IMG_Size])[Batch_Size*(i):Batch_Size*(i+1),:]
+            labels = data[1][Batch_Size*(i):Batch_Size*(i+1),:]
+            '''
+            if i%20 == 0:
+                print(labels[0,:])
+                print(train[0].reshape([28,28]).shape)
+                plt.imshow(train[0].reshape([28,28]))
+                plt.show()
+            '''
+            layer_1_ = train.dot(weight_1)+bias_1
+            layer_1 = sigmoid(layer_1_)
+            layer_2_ = layer_1.dot(weight_2)+bias_2
+            layer_2 = sigmoid(layer_2_)
+
+            assume = np.argmax(layer_2,axis=1)
+            labels_ = np.argmax(labels,axis=1)
+            ratio = np.count_nonzero(assume == labels_)/Batch_Size
+
+            loss = MSE(layer_2,labels)
+            #print(layer_2)
+            #print(labels)
+            #print((layer_2-labels)**2)
+            print(loss)
+            print(ratio)
+            loss_pack.append(loss)
+            ratio_pack.append(ratio)
+
+            delta = Learning_Rate
+            delta = MSE_backward(delta,layer_2-labels)
+            delta = sigmoid_backward(delta,layer_2)
+            delta_weight_2 = layer_1.T.dot(delta)
+            delta_bias_2 = np.sum(delta,0)
+            delta = delta.dot(weight_2.T)
+
+            delta = sigmoid_backward(delta,layer_1)
+            delta_weight_1 = train.T.dot(delta)
+            delta_bias_1 = np.sum(delta,0)
+
+            weight_1 -= delta_weight_1
+            weight_2 -= delta_weight_2
+
+    test = data[2].reshape([-1,IMG_Size])
+    labels = data[3]
+
+    print(test.shape)
+    print(labels.shape)
+    Test_size = test.shape[0]
+
+    layer_1_ = test.dot(weight_1)+bias_1
+    layer_1 = sigmoid(layer_1_)
+    layer_2_ = layer_1.dot(weight_2)+bias_2
+    layer_2 = sigmoid(layer_2_)
+
+    loss = MSE(layer_2,labels)
+
+    assume = np.argmax(layer_2,axis=1)
+    labels_ = np.argmax(labels,axis=1)
+    ratio = np.count_nonzero(assume == labels_)/Test_size
+
+    print(loss)
+    print(ratio)
+    plt.plot(loss_pack)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
